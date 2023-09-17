19fe45b95354e2dd64e9cee03bb48a4da94db253
msu32220223221401205 <144470804+msu32220223221401205@users.noreply.github.com>
2023-09-17 09:27:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,45 +5,25 @@
 
 
 
-class BankAccount:
-  
- def __init__(self, account_number, account_holder_name, initial_balance = 0.0):
-  self.__account_number = account_number
-  self.__account_holder_name = account_holder_name
-  self.__account_balance = initial_balance
+#define the base class player
+class Player:
+  def play(self):
+    print("the player is playing cricket")
 
- def deposit(self, amount):
-   if amount > 0:
-     self.__account_balance += amount
-     print("deposited ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+#define the derived class batsman
+class Batsman(Player):
+  def play(self):
+    print("the Batsman is batting ")
 
-   else:
-    print("invalid deposit amount.please deposit a positive amount.")
+#define the derived class bowler
+class Bowler(Player):
+  def play(self):
+    print("the bowler is bowling ")
 
- def withdraw (self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-       self.__account_balance -= amount
-       print("withdrew ₹{}. New balance: ₹ ()".format(amount,self.__account_balance)) 
+#create objects of batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler ()
 
-    else:
-     print("invalid withdrawal amount or insufficient balance.")
-
- def display_balance(self):
-   print("account balance for {} (Account #{}): ₹{}".format(
-    self.__account_holder_name, self.__account_number,
-   self.__account_balance))
-        
-
-#create an instantce of the BankAccount class
-account = BankAccount(account_number ="123456789",
-                  account_holder_name="hari prasad",
-initial_balance=5000.0)
-
-#test deposit and withdrawal functionality 
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
-account.display_balance()
-
-
+#call the play() method for each object
+batsman.play()
+bowler.play()
